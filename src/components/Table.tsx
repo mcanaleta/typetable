4b484d40fe89b77f08f4d79851c59e71bb3158f6
Marc Canaleta <mcanaleta@gmail.com>
2023-11-02 22:24:34 +0100

    storybook and table wip

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,68 @@
 import React from "react";
 import styled from "styled-components";
-import { TableProps } from "./Table.types";
+import { Column } from "./column";
 
 const StyledTable = styled.table<TableProps>`
   border: 1;
+  border-collapse: collapse;
 `;
 
+const StyledTh = styled.th<{ $width?: string }>`
+  background-color: #f2f2f2;
+  border-color: silver;
+  border-style: solid;
+  border-width: 1px;
+  padding-inline: 5px;
+  text-transform: uppercase;
+  font-size: 0.8em;
+  cursor: default;
+  width: ${(props) => props.$width};
+`;
+
+const StyledTd = styled.td<{ $width?: string }>`
+  border-color: lightgrey;
+  border-style: solid;
+  border-width: 1px;
+  padding-inline: 5px;
+  cursor: default;
+  width: ${(props) => props.$width};
+`;
+
+export interface TableProps {
+  /**
+   * Table data
+   */
+  data: any[];
+
+  /**
+   * Table header
+   */
+  columns: Column[];
+
+  editable: boolean;
+}
+
+/**
+ * Table component
+ */
 export function Table(props: TableProps) {
   return (
     <StyledTable {...props}>
-      <thead></thead>
+      <thead>
+        {props.columns.map((column, index) => (
+          <StyledTh key={index} $width={column.width}>
+            {column.name}
+          </StyledTh>
+        ))}
+      </thead>
       <tbody>
         {props.data.map((row, index) => (
           <tr key={index}>
-            <td>{JSON.stringify(row)}</td>
+            {props.columns.map((column, index) => (
+              <StyledTd key={index} $width={column.width}>
+                {row[column.name]}
+              </StyledTd>
+            ))}
           </tr>
         ))}
       </tbody>
